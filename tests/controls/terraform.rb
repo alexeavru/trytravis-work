fc2234d21b918a9530c478f19021f33c148c8441
Ermolaev Aleksey <eav444@gmail.com>
2018-11-08 22:55:20 +0300

    FIX

--- tests/controls/terraform.rb
+++ tests/controls/terraform.rb
@@ -7,11 +7,7 @@ control 'terraform' do
   impact 1
   title '**** Run terraform validation ****s'
 
-  describe command('cd terraform/stage; terraform validate -var-file=terraform.tfvars.example') do
-    its('exit_status') { should eq 0 }
-  end
-
-  describe command('cd ../prod; terraform validate -var-file=terraform.tfvars.example') do
+  describe command('terraform validate -var-file=terraform.tfvars.example terraform/stage/') do
     its('exit_status') { should eq 0 }
   end
 
