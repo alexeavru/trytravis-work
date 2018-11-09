ad7719f2db44ede4b528c3eb91c04c1c6191e84b
Ermolaev Aleksey <eav444@gmail.com>
2018-11-09 15:34:28 +0300

    FIX

--- run2.sh
+++ run2.sh
@@ -6,6 +6,7 @@ echo 'Install Packer'
 echo '*************************************************************'
 # Install packer
 sudo rm -f packer_*.zip
+sudo rm -f /usr/local/bin/packer
 sudo wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_386.zip
 sudo unzip packer_1.3.1_linux_386.zip
 sudo rm -f packer_1.3.1_linux_386.zip
