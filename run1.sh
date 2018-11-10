1f36b9ee49f2d3fdf9c8c31f6d5655df737e5764
Ermolaev Aleksey <eav444@gmail.com>
2018-11-10 20:55:30 +0300

    132

--- run1.sh
+++ run1.sh
@@ -7,10 +7,20 @@ echo '*************************************************************'
 echo 'Install Packer'
 echo '*************************************************************'
 # Install packer
+
+echo 'rm /usr/local/bin/packer'
 sudo rm -f /usr/local/bin/packer
+
+echo 'wget packer'
 sudo wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
+
+echo 'unzip packer'
 sudo unzip packer_*.zip
+
+echo 'rm packer ZIP'
 sudo rm -f packer_*.zip
+
+echo 'MV PACKER'
 sudo mv packer /usr/local/bin/
 
 
