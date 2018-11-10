c11858775df070e5edfbdf9fb2d638cdd5b61b50
Ermolaev Aleksey <eav444@gmail.com>
2018-11-10 21:15:28 +0300

    134

--- run1.sh
+++ run1.sh
@@ -10,21 +10,27 @@ echo '*************************************************************'
 
 echo 'rm /usr/local/bin/packer'
 sudo rm -f /usr/local/bin/packer
+echo $?
 
 echo 'wget packer'
 sudo wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
+echo $?
 
 echo 'unzip packer'
 sudo unzip packer_*.zip
+echo $?
 
 echo 'rm packer ZIP'
 sudo rm -f packer_*.zip
+echo $?
 
 echo 'MV PACKER'
 sudo mv packer /usr/local/bin/
+echo $?
 
 PACKER=`packer -v`
 echo $PACKER
+echo $?
 
 echo 'Step 111111111111111111111111111111111111111'
 
