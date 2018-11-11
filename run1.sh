0eea81ac7322a7a8b32dc24f6966ec9bccf5c098
Ermolaev Aleksey <eav444@gmail.com>
2018-11-11 21:29:41 +0300

    149

--- run1.sh
+++ run1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 PROJECT_ROOT=`pwd`
 
 cd /usr/local/src
@@ -38,7 +39,7 @@ sudo wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_386.zip
 sudo unzip packer_1.3.1_linux_386.zip
 sudo rm -f packer_1.3.1_linux_386.zip
 sudo mv packer /usr/local/bin/
-packer -v &
+packer -v
 
 cd $PROJECT_ROOT
 
@@ -54,13 +55,13 @@ echo '*************************************************************'
 echo 'RUN PACKER TESTS'
 echo '*************************************************************'
 cd $PROJECT_ROOT
-packer validate -var-file=packer/variables.json.example packer/app.json &
+packer validate -var-file=packer/variables.json.example packer/app.json
 echo $?
-packer validate -var-file=packer/variables.json.example packer/db.json &
+packer validate -var-file=packer/variables.json.example packer/db.json
 echo $?
-packer validate -var-file=packer/variables.json.example packer/immutable.json &
+packer validate -var-file=packer/variables.json.example packer/immutable.json
 echo $?
-packer validate -var-file=packer/variables.json.example packer/ubuntu16.json &
+packer validate -var-file=packer/variables.json.example packer/ubuntu16.json
 echo $?
 
 echo '*************************************************************'
