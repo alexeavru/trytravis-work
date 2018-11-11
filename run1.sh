c589544291855a05fac8c5d8a15d90c8822ecb18
Ermolaev Aleksey <eav444@gmail.com>
2018-11-11 20:20:27 +0300

    141

--- run1.sh
+++ run1.sh
@@ -4,50 +4,81 @@ PROJECT_ROOT=`pwd`
 cd /usr/local/src
 
 echo '*************************************************************'
+echo 'Install Ansible-lint'
+echo '*************************************************************'
+# Install Ansible-lint
+sudo pip install ansible-lint
+ansible-lint --version
+
+echo '*************************************************************'
+echo 'Install Terraform'
+echo '*************************************************************'
+# Install Terraform
+sudo wget https://releases.hashicorp.com/terraform/0.11.9/terraform_0.11.9_linux_386.zip
+sudo unzip terraform_*.zip
+sudo rm -f terraform_*.zip
+sudo mv /usr/local/src/terraform /usr/local/bin/
+terraform -v
+
+echo '*************************************************************'
+echo 'Install Tflint'
+echo '*************************************************************'
+# Install tflint
+sudo wget https://github.com/wata727/tflint/releases/download/v0.7.2/tflint_linux_386.zip
+sudo unzip tflint_*.zip
+sudo rm -f tflint_*.zip
+sudo mv /usr/local/src/tflint /usr/local/bin/
+tflint -v
+
+echo '*************************************************************'
 echo 'Install Packer'
 echo '*************************************************************'
 # Install packer
-
-echo 'rm /usr/local/bin/packer'
-sudo rm -f /usr/local/bin/packer
-echo $?
-
-echo 'wget packer'
-sudo wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
-echo $?
-
-echo 'unzip packer'
-sudo unzip packer_*.zip
-echo $?
-
-echo 'rm packer ZIP'
-sudo rm -f packer_*.zip
-echo $?
-
-echo 'MV PACKER'
+sudo wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_386.zip
+sudo unzip packer_1.3.1_linux_386.zip
+sudo rm -f packer_1.3.1_linux_386.zip
 sudo mv packer /usr/local/bin/
-echo $?
+exec packer -v &
 
-echo 'RUN PACKER --version'
-exec /usr/local/bin/packer -v &
-echo $?
-
-echo 'Step 111111111111111111111111111111111111111'
 
 cd $PROJECT_ROOT
+
+# Run TESTS
+echo '**** RUN TESTS ****'
+
+echo '*************************************************************'
+echo 'RUN ANSIBLE-LINT TESTS'
+echo '*************************************************************'
+ansible-lint --exclude=roles/jdauphant.nginx ansible/playbooks/*.yml
+
+echo '*************************************************************'
+echo 'RUN TERRAFORM TEST'
+echo '*************************************************************'
+cd $PROJECT_ROOT/terraform/stage
+rm backend.tf
+mv terraform.tfvars.example terraform.tfvars
+terraform get
+terraform init
+terraform validate -check-variables=false
+tflint
+
+cd $PROJECT_ROOT/terraform/prod
+rm backend.tf
+mv terraform.tfvars.example terraform.tfvars
+terraform get
+terraform init
+terraform validate -check-variables=false
+tflint
 
 echo '*************************************************************'
 echo 'RUN PACKER TESTS'
 echo '*************************************************************'
 cd $PROJECT_ROOT
 packer validate -var-file=packer/variables.json.example packer/app.json
-echo $?
 packer validate -var-file=packer/variables.json.example packer/db.json
-echo $?
 packer validate -var-file=packer/variables.json.example packer/immutable.json
-echo $?
 packer validate -var-file=packer/variables.json.example packer/ubuntu16.json
-echo $?
+
 
 echo '*************************************************************'
 echo 'END TESTS'
