7186806ed49e42b119d5a38c05b55f1c97ee9f62
Ermolaev Aleksey <eav444@gmail.com>
2018-11-11 20:54:02 +0300

    144

--- run1.sh
+++ run1.sh
@@ -38,8 +38,8 @@ sudo wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_386.zip
 sudo unzip packer_1.3.1_linux_386.zip
 sudo rm -f packer_1.3.1_linux_386.zip
 sudo mv packer /usr/local/bin/
-exec packer -v &
-
+OUTPUT="$(packer -v)"
+echo $OUTPUT
 
 cd $PROJECT_ROOT
 
